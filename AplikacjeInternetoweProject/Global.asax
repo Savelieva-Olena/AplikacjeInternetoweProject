70f0addf7c8b8c9b9952d17149d06870ade0ea37
Olena Savelieva <savelieva372@gmail.com>
2020-01-24 13:49:08 +0100

    Add project files.

---
+++ AplikacjeInternetoweProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AplikacjeInternetoweProject.MvcApplication" Language="C#" %>
---
+++ AplikacjeInternetoweProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AplikacjeInternetoweProject.Startup))]
+namespace AplikacjeInternetoweProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
